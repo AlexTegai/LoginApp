d7890057dad5a91fd9dd9906848badbd8a57fb6d
Aleksandr Tegai <shura.tegay@mail.ru>
2021-07-06 13:02:13 +0300

    Initial Commit

---
+++ HomeWork 2.4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork 2.4
+//
+//  Created by Alex Tegai on 06.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
